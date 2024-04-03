b75186083289fbe34fe61fdec7ddb168e46615df
aburke76 <alexlburke8771@gmail.com>
2024-04-03 17:32:23 -0400

    imported createProject

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
 import "../styles/frame.css";
+import { addProject } from "../scripts/createProject";
+
+const newProject = document.querySelector("#add-project");
+
+newProject.addEventListener("click", () => {
+    addProject();
+});
