38cfd61fd0723e9094065297c3e22c291d40c9fd
aburke76 <alexlburke8771@gmail.com>
2024-04-06 09:58:14 -0400

    added function to change DOM for projects

--- src/scripts/createProject.js
+++ src/scripts/createProject.js
@@ -4,7 +4,25 @@ class Project {
     }
 }
 
+const projectModal = document.querySelector("#project-modal");
+
 export function addProject() {
-    const projectModal = document.querySelector("#project-modal");
     projectModal.showModal();
+}
+
+export function listNewProject() {
+    const projectTitle = document.querySelector("#project-title");
+    const sidebarContent = document.querySelector("#sidebar-content");
+    const projectDiv = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const newProject = new Project(projectTitle.value);
+
+    sidebarContent.append(projectDiv);
+    h3.textContent = newProject.name;
+    projectDiv.append(h3);
+
+    projectModal.close();
+    console.log(newProject);
+
+    projectTitle.value = "";
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,15 @@
 import "../styles/frame.css";
 import { addProject } from "../scripts/createProject";
+import { listNewProject } from "../scripts/createProject";
 
 const newProject = document.querySelector("#add-project");
+const submitBtn = document.querySelector("#submit-project");
 
 newProject.addEventListener("click", () => {
     addProject();
+});
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    listNewProject();
 });
