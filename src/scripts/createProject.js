122a36a69ddaa3c0943ead6a9e372c82ebac8305
aburke76 <alexlburke8771@gmail.com>
2024-04-07 09:45:20 -0400

    redoing some code

--- src/scripts/createProject.js
+++ src/scripts/createProject.js
@@ -1,3 +1,5 @@
+import "../styles/sidebar.css";
+
 class Project {
     constructor(name) {
         this.name = name;
@@ -8,21 +10,4 @@ const projectModal = document.querySelector("#project-modal");
 
 export function addProject() {
     projectModal.showModal();
-}
-
-export function listNewProject() {
-    const projectTitle = document.querySelector("#project-title");
-    const sidebarContent = document.querySelector("#sidebar-content");
-    const projectDiv = document.createElement("div");
-    const h3 = document.createElement("h3");
-    const newProject = new Project(projectTitle.value);
-
-    sidebarContent.append(projectDiv);
-    h3.textContent = newProject.name;
-    projectDiv.append(h3);
-
-    projectModal.close();
-    console.log(newProject);
-
-    projectTitle.value = "";
 }
