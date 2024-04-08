3f413feb1a3777dd48a68938a53102e941600f07
aburke76 <alexlburke8771@gmail.com>
2024-04-08 17:28:22 -0400

    default project displays, can be removed

--- src/scripts/createProject.js
+++ src/scripts/createProject.js
@@ -1,20 +1,26 @@
 import "../styles/sidebar.css";
 
+const projectList = [];
+
 class Project {
     constructor(name) {
         this.name = name;
-        this.list = [];
+        this.taskList = [];
     }
 
-    addToDont(task) {
-        this.list.push(task);
+    addProject() {
+        projectList.push(this);
     }
 
-    remove(task) {
-        const index = this.list.indexOf(task);
-        if (index !== -1) {
-            this.list.splice(index, 1);
+    deleteProject() {
+        const index = projectList.indexOf(this);
+        if (index > -1) {
+            projectList.splice(index, 1);
         }
+    }
+
+    addTask(task) {
+        this.taskList.push(task);
     }
 }
 
@@ -27,8 +33,45 @@ class Task {
     }
 }
 
+const defaultProject = new Project("Default");
+
+const defaultTask = new Task(
+    "Don't Cry",
+    "keep it together, man",
+    "04/09/2024",
+    "High"
+);
+
+defaultProject.addProject();
+defaultProject.addTask(defaultTask);
+console.log(projectList);
+
 const projectModal = document.querySelector("#project-modal");
 
 export function addProject() {
     projectModal.showModal();
+}
+
+export function displayProjects() {
+    const content = document.querySelector("#sidebar-content");
+
+    projectList.forEach((el) => {
+        const projectDiv = document.createElement("div");
+        const delBtn = document.createElement("button");
+        projectDiv.classList.add("project-div");
+        content.append(projectDiv);
+        const name = document.createElement("h3");
+        name.setAttribute("id", "project-name");
+        name.textContent = el.name;
+        projectDiv.append(name);
+        delBtn.textContent = "-";
+        projectDiv.append(delBtn);
+
+        delBtn.addEventListener("click", () => {
+            el.deleteProject();
+            console.log(projectList);
+
+            projectDiv.remove();
+        });
+    });
 }
