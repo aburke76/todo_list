f0e7c81ff04eeb05e632eabcf64f7197341ed8ce
aburke76 <alexlburke8771@gmail.com>
2024-04-20 12:49:22 -0400

    worked on adding tasks to projects

--- src/scripts/manageProject.js
+++ src/scripts/manageProject.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { displayProject } from "./displayProject";
+import { createTask } from "./manageTask";
 
 class ProjectManager {
     allProjects = [];
@@ -26,26 +27,40 @@ export class Project {
     }
 
     addTask(task) {
+        task.projectId = this.id;
         this.taskList.push(task);
+    }
+
+    removeTask(id) {
+        this.taskList = this.taskList.filter((el) => {
+            return el.id !== id;
+        });
     }
 }
 
 export class Task {
-    constructor(name, notes, dueDate, priority, id = uuidv4()) {
+    constructor(name, notes, dueDate, priority, taskId = uuidv4()) {
         this.name = name;
         this.notes = notes;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.id = id;
+        this.taskId = taskId;
     }
 }
 
 export const projectList = new ProjectManager();
 
 const defaultProject = new Project("Default");
+defaultProject.active = true;
 projectList.addProject(defaultProject);
-defaultProject.addTask("Don't Cry", "keep it together, man", "01/01/1999");
-console.log(projectList.allProjects);
+const defaultTask = new Task(
+    "Don't Cry",
+    "keep it together, man",
+    "01/01/1999",
+    "High"
+);
+
+defaultProject.addTask(defaultTask);
 
 export function openProjectModal() {
     const modal = document.querySelector("#project-modal");
@@ -57,15 +72,21 @@ export function closeProjectModal() {
     modal.close();
 }
 
+function clearProjectModal() {
+    const userInput = document.querySelector("#project-title");
+    userInput.value = null;
+}
+
 export function createProject() {
     const userInput = document.querySelector("#project-title");
     const newProject = new Project(userInput.value);
     projectList.addProject(newProject);
     displayProject();
+    clearProjectModal();
     closeProjectModal();
-    userInput.value = null;
-    console.log(projectList.allProjects);
 }
+
+export function selectProject() {}
 
 export function deleteProject(projectId) {
     const projectDivs = document.querySelectorAll(".project-div");
--- src/scripts/manageTask.js
+++ src/scripts/manageTask.js
@@ -1,16 +1,23 @@
-import { v4 as uuidv4 } from "uuid";
-import { projectList } from "./manageProject";
+import { projectList, Task, Project } from "./manageProject";
+import { displayTask } from "./displayTask";
 
 export function openTaskModal() {
     const modal = document.querySelector("#task-modal");
     modal.showModal();
 }
 
+export function closeTaskModal() {
+    const modal = document.querySelector("#task-modal");
+    modal.close();
+}
+
 export function createTask() {
+    Project;
     const name = document.querySelector("#task-name");
     const notes = document.querySelector("#task-notes");
     const date = document.querySelector("#task-dd");
     const priority = document.querySelector("#task-priority");
+    const projectDivs = document.querySelectorAll(".project-div");
 
     const newTask = new Task(
         name.value,
@@ -18,4 +25,37 @@ export function createTask() {
         date.value,
         priority.value
     );
+
+    projectDivs.forEach((el) => {
+        let elId;
+        if (el.classList.contains("active")) {
+            elId = el.id;
+        }
+        console.log(projectList.allProjects);
+
+        for (let i = 0; i < projectList.allProjects.length; i++) {
+            let selectedProject;
+            if (projectList.allProjects[i].id === elId) {
+                selectedProject = i;
+                projectList.allProjects[i].addTask(newTask);
+            }
+        }
+
+        console.log(projectList.allProjects);
+    });
+
+    closeTaskModal();
+    clearTaskModal();
+    displayTask();
+}
+
+function clearTaskModal() {
+    const name = document.querySelector("#task-name");
+    const notes = document.querySelector("#task-notes");
+    const date = document.querySelector("#task-dd");
+    const priority = document.querySelector("#task-priority");
+    name.value = null;
+    notes.value = null;
+    date.value = null;
+    priority.value = null;
 }
