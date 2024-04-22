fe826e0daf44efc6f82bdf1ab19367262721b836
aburke76 <alexlburke8771@gmail.com>
2024-04-22 09:57:21 -0400

    created new functions tmanage tasks

--- src/scripts/manageTask.js
+++ src/scripts/manageTask.js
@@ -1,5 +1,5 @@
-import { projectList, Task, Project } from "./manageProject";
-import { displayTask } from "./displayTask";
+import { projectList, Task, Project, deleteProject } from "./manageProject";
+import { displayTask, removeTask } from "./displayTask";
 
 export function openTaskModal() {
     const modal = document.querySelector("#task-modal");
@@ -12,12 +12,10 @@ export function closeTaskModal() {
 }
 
 export function createTask() {
-    Project;
     const name = document.querySelector("#task-name");
     const notes = document.querySelector("#task-notes");
     const date = document.querySelector("#task-dd");
     const priority = document.querySelector("#task-priority");
-    const projectDivs = document.querySelectorAll(".project-div");
 
     const newTask = new Task(
         name.value,
@@ -26,27 +24,108 @@ export function createTask() {
         priority.value
     );
 
-    projectDivs.forEach((el) => {
-        let elId;
-        if (el.classList.contains("active")) {
-            elId = el.id;
-        }
-        console.log(projectList.allProjects);
-
-        for (let i = 0; i < projectList.allProjects.length; i++) {
-            let selectedProject;
-            if (projectList.allProjects[i].id === elId) {
-                selectedProject = i;
-                projectList.allProjects[i].addTask(newTask);
-            }
-        }
-
-        console.log(projectList.allProjects);
-    });
+    activeProjectId();
+    addTask(newTask);
 
     closeTaskModal();
     clearTaskModal();
-    displayTask();
+    displayTask(newTask);
+    console.log(newTask.taskId);
+}
+
+export function activeProjectId() {
+    const projectDivs = document.querySelectorAll(".project-div");
+    let divId;
+    projectDivs.forEach((div) => {
+        if (div.classList.value.includes("active")) {
+            divId = div.id;
+        }
+    });
+    return divId;
+}
+
+function addTask(task) {
+    const elId = activeProjectId();
+    for (let i = 0; i < projectList.allProjects.length; i++) {
+        if (projectList.allProjects[i].id === elId) {
+            projectList.allProjects[i].addTask(task);
+        }
+    }
+}
+
+export function findTaskList() {
+    const projectId = activeProjectId();
+    let selectedProject;
+    let currentTaskList;
+    for (let i = 0; i < projectList.allProjects.length; i++) {
+        if (projectList.allProjects[i].id === projectId) {
+            selectedProject = i;
+        }
+    }
+    currentTaskList = projectList.allProjects[selectedProject].taskList;
+    return currentTaskList;
+}
+
+export function findTask() {
+    const projectId = activeProjectId();
+    let selectedProject;
+    let currentTask;
+    for (let i = 0; i < projectList.allProjects.length; i++) {
+        if (projectList.allProjects[i].id === projectId) {
+            selectedProject = i;
+        }
+    }
+    for (
+        let i = 0;
+        i < projectList.allProjects[selectedProject].taskList.length;
+        i++
+    ) {
+        if (
+            projectList.allProjects[selectedProject].taskList[i].projectId ===
+            projectId
+        ) {
+            currentTask = projectList.allProjects[selectedProject].taskList[i];
+        }
+    }
+    return currentTask;
+}
+
+export function findProject() {
+    const projectId = activeProjectId();
+    let currentProject;
+    for (let i = 0; i < projectList.allProjects.length; i++) {
+        if (projectList.allProjects[i].id === projectId) {
+            currentProject = i;
+        }
+    }
+    return projectList.allProjects[currentProject];
+}
+
+export function deleteTask(taskId) {
+    //TAKING OUT NEWEST TASK
+    const currentProject = findProject();
+    const taskList = findTaskList();
+    let selectedTaskId;
+
+    taskList.forEach((task) => {
+        if (task.taskId === taskId) {
+            selectedTaskId = taskId;
+        }
+    });
+    const delBtns = document.querySelectorAll(".task-del-btn");
+    delBtns.forEach((btn) => {
+        if (btn.id === selectedTaskId) {
+            currentProject.removeTask(taskId);
+        }
+    });
+
+    const taskDivs = document.querySelectorAll(".task-div");
+    taskDivs.forEach((div) => {
+        if (div.id === selectedTaskId) {
+            div.remove();
+        }
+    });
+    console.log(taskList);
 }
 
 function clearTaskModal() {
